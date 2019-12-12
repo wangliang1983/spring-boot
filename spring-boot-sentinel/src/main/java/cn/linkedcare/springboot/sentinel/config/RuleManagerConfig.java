2afbf7a3e8e8de478ccf1de08f42f85a74573201
yangke250 <yangke250@DESKTOP-AER1DMF>
2019-12-12 11:11:37 +0800

    初始化

--- spring-boot-sentinel/src/main/java/cn/linkedcare/springboot/sentinel/config/RuleManagerConfig.java
+++ spring-boot-sentinel/src/main/java/cn/linkedcare/springboot/sentinel/config/RuleManagerConfig.java
@@ -1,5 +1,29 @@
 package cn.linkedcare.springboot.sentinel.config;
 
-public class RuleManagerConfig {
+import java.util.List;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextStartedEvent;
+import org.springframework.stereotype.Component;
+
+import com.alibaba.csp.sentinel.slots.block.degrade.DegradeRule;
+import com.alibaba.csp.sentinel.slots.block.degrade.DegradeRuleManager;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
+
+import cn.linkedcare.springboot.sentinel.listener.SentinelDegradeRuleListener;
+import cn.linkedcare.springboot.sentinel.listener.SentinelFlowRuleListener;
+
+@Component
+public class RuleManagerConfig implements ApplicationListener<ContextStartedEvent>{
+
+	public void onApplicationEvent(ContextStartedEvent event) {
+		List<DegradeRule>  degradeRules = SentinelDegradeRuleListener.getRules();
+		DegradeRuleManager.loadRules(degradeRules);
+		
+		List<FlowRule> flowRules = SentinelFlowRuleListener.getRules();
+		FlowRuleManager.loadRules(flowRules);
+	}
 
 }
